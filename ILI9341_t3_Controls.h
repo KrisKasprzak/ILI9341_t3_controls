46e6ee35415a0e65ac2c06518646bce3542f3309
Kris Kasprzak <kris.kasprzak@yahoo.com>
2021-04-07 08:46:08 -0500

    Update ILI9341_t3_Controls.h

--- ILI9341_t3_Controls.h
+++ ILI9341_t3_Controls.h
@@ -1,33 +1,36 @@
 /*
-The MIT License (MIT)
 
-library writen by Kris Kasprzak
+	The MIT License (MIT)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	library writen by Kris Kasprzak
 
-On a personal note, if you develop an application or product using this library
-and make millions of dollars, I'm happy for you!
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-rev	date	author		change
-1.0	9/2019	kasprzak	initial code
-2.0	9/2020	kasprzak	added shapes and sizes for handles
-3.0	10/2020	kasprzak	fixed some repaint issues in CGraph, added marker support, added Button class with tons of methods
-4.0	11/2020	kasprzak	fixed bugs added Button, Checkbox, OptionButton classes
-5.0	11/2020	kasprzak	modified sliders, option and check to return true/false if pressed, and actual value stored in value property
-5.1	4/2021	kasprzak	added changed() back into sliderH and SliderV improved touch/control accuracy
+	On a personal note, if you develop an application or product using this library
+	and make millions of dollars, I'm happy for you!
+
+	rev	date	author		change
+	1.0	9/2019	kasprzak	initial code
+	2.0	9/2020	kasprzak	added shapes and sizes for handles
+	3.0	10/2020	kasprzak	fixed some repaint issues in CGraph, added marker support, added Button class with tons of methods
+	4.0	11/2020	kasprzak	fixed bugs added Button, Checkbox, OptionButton classes
+	5.0	11/2020	kasprzak	modified sliders, option and check to return true/false if pressed, and actual value stored in value property
+	5.1	4/2021	kasprzak	added changed() back into sliderH and SliderV improved touch/control accuracy
+
+	5.3	4/2021	kasprzak		added redraw code to the change handle size and shape, that way the old gets painted out upon a change (after init());
 */
 
 
