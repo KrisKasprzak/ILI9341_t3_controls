a10f85d02776714f10c11bde19c888db72abb8a3
Kris Kasprzak <kris.kasprzak@yahoo.com>
2022-09-09 11:27:44 -0500

    Update OptionButtons.ino

--- Examples/OptionButtons/OptionButtons.ino
+++ Examples/OptionButtons/OptionButtons.ino
@@ -41,6 +41,7 @@
 // step 1 include the library
 #include <ILI9341_t3_Controls.h>  // custom control define file
 #include <font_Arial.h>
+#include <Colors.h>
 
 #define FONT Arial_16
 #define TFT_DC 9       // DC pin on LCD
