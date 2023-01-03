da4ef316504e7edb1ca99e1571ad74aade3abc30
Kris Kasprzak <kris.kasprzak@yahoo.com>
2023-01-03 16:00:06 -0600

    Update ILI9341_t3_Controls.cpp

--- ILI9341_t3_Controls.cpp
+++ ILI9341_t3_Controls.cpp
@@ -29,8 +29,8 @@
 	5.0		11/2020			kasprzak			modified sliders, option and check to return true/false if pressed, and actual value stored in value property
 	5.1		11/2020			kasprzak			added automatic "blank out" old handle support insided draw() method in sliderH and SliderV (really needed when a slide is redrawn based on input other than a finger slide (encoder)
 	5.4		12/2021			kasprzak			added ring sliders 
-	5.5 	11/2022			kasprzak			added better text centering control
-	5.5 	1/2023			kasprzak			added icons for buttons
+	5.5 		11/2022			kasprzak			added better text centering control
+	5.6 		1/2023			kasprzak			added icons for buttons
 	
 */
 
