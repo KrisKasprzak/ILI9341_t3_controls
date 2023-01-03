fb44ac01a4301b236609aa61e61dc10ed70a5b10
Kris Kasprzak <kris.kasprzak@yahoo.com>
2023-01-03 15:27:46 -0600

    Update Buttons_WithIcons.ino

--- Examples/Buttons_WithIcons/Buttons_WithIcons.ino
+++ Examples/Buttons_WithIcons/Buttons_WithIcons.ino
@@ -359,7 +359,14 @@ void setup() {
   Touch.begin();
 
   Touch.setRotation(3);
-  ButtonIcon.init(180, 140, 56, 56, C_BLACK, C_BLACK, C_BLACK, C_BLACK, Settings, Settings_Pressed, 50, 50, 3, 3);
+
+/*
+init(int16_t ButtonX, int16_t ButtonY, uint8_t ButtonWidth, uint8_t ButtonHeight,
+		uint16_t OutlineColor, uint16_t ButtonColor, uint16_t BackgroundColor,
+		const uint16_t  *UpIcon, const uint16_t *DnIcon, int16_t IconWidth, int16_t IconHeight, int OffsetLeft, int OffsetTop ) {
+*/
+				
+  ButtonIcon.init(180, 140, 56, 56, C_BLACK, C_BLACK, C_BLACK, Settings, Settings_Pressed, 50, 50, 3, 3);
   ButtonIcon.setCornerRadius(2);
 
   ButtonIcon.draw();
