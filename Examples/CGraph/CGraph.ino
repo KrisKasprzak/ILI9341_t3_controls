b05aae2116ba4e77dbeeeaad7d7944ee20eb7dac
Kris Kasprzak <kris.kasprzak@yahoo.com>
2023-03-25 20:13:10 -0500

    Update CGraph.ino

--- Examples/CGraph/CGraph.ino
+++ Examples/CGraph/CGraph.ino
@@ -1,7 +1,6 @@
 #include <ILI9341_t3.h>           // fast display driver lib
 #include <ILI9341_t3_Controls.h>
 #include <font_Arial.h>           // custom fonts that ships with ILI9341_t3.h
-#include <Colors.h>
 
 // you must create and pass fonts to the function
 #define FONT_TITLE Arial_16
@@ -25,6 +24,61 @@ ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 #define Y_LOSCALE 	-1
 #define Y_HISCALE 	1
 #define Y_INC 	    0.2
+
+// http://www.barth-dev.de/online/rgb565-color-picker/
+
+
+#define C_WHITE   		0xFFFF
+#define	C_BLACK   		0x0000
+#define C_GREY       	0xC618
+#define	C_BLUE    		0x001F
+#define	C_RED     		0xF800
+#define	C_GREEN   		0x07E0
+#define C_CYAN    		0x07FF
+#define C_MAGENTA 		0xF81F
+#define C_YELLOW  		0xFFE0  
+#define C_TEAL			0x0438       
+#define C_ORANGE      	0xDC00          
+#define C_PINK        	0xF81F
+#define C_PURPLE		0x801F
+
+
+#define C_LTGREY      	0xE71C  
+#define C_LTBLUE		0x73DF    
+#define C_LTRED       	0xFBAE
+#define C_LTGREEN     	0x7FEE
+#define C_LTCYAN		0x77BF
+#define C_LTMAGENTA   	0xFBB7
+#define C_LTYELLOW    	0xF7EE
+#define C_LTTEAL		0x77FE  
+#define C_LTORANGE    	0xFDEE
+#define C_LTPINK      	0xFBBA
+#define C_LTPURPLE		0xD3BF
+
+//#define C_DKGREY      	0x2124
+#define C_DKGREY      	0x52AA
+#define C_DKBLUE      	0x080B
+#define C_DKRED       	0x7800
+#define C_DKGREEN     	0x03C2   
+#define C_DKCYAN      	0x032F  
+#define C_DKMAGENTA   	0x900B
+#define C_DKYELLOW    	0x94A0
+#define C_DKTEAL		0x0452
+#define C_DKORANGE   	0x92A0         
+#define C_DKPINK      	0x9009
+#define C_DKPURPLE    	0x8012  
+ 
+#define C_MDGREY      	0x7BCF  
+#define C_MDBLUE		0x1016
+#define	C_MDRED     	0xB000
+#define	C_MDGREEN   	0x0584
+#define C_MDCYAN    	0x04B6
+#define C_MDMAGENTA 	0xB010
+#define C_MDYELLOW      0xAD80	  
+#define C_MDTEAL		0x0594     
+#define C_MDORANGE      0xB340           
+#define C_MDPINK        0xB00E
+#define C_MDPURPLE		0x8816 
 
 #define TEXTCOLOR C_WHITE
 #define GRIDCOLOR C_GREY
